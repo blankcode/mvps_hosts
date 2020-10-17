5a8981aafa87693c345ba21d8a2d7797d66c0722
Brian Blankenship <brianblankenship1978@gmail.com>
2020-10-16 21:28:29 -0400

    more comment formating

--- mvsp-update.sh
+++ mvsp-update.sh
@@ -7,12 +7,12 @@
 #   Add a cron job to update regularly.
 ########
 
-mkdir /etc/tmpMVP #Make a tmp dir
-cd /etc/tmpMVP #go to tmp dir
-curl http://winhelp2002.mvps.org/hosts.zip > hosts.zip #download the hosts file from mvps and save to hosts.zip
-unzip hosts.zip #inflate hosts.zip
-cat HOSTS > /etc/hosts #replace the /etc/hosts file with the new one
-rm -rf /etc/tmpMVP #remove the tmp dir
-dscacheutil -flushcache #reset the DNS Cache and initialize the new hosts file
+mkdir /etc/tmpMVP # Make a tmp dir
+cd /etc/tmpMVP # go to tmp dir
+curl http://winhelp2002.mvps.org/hosts.zip > hosts.zip # download the hosts file from mvps and save to hosts.zip
+unzip hosts.zip # inflate hosts.zip
+cat HOSTS > /etc/hosts # replace the /etc/hosts file with the new one
+rm -rf /etc/tmpMVP # remove the tmp dir
+dscacheutil -flushcache # reset the DNS Cache and initialize the new hosts file
 
-exit 0 #Exit and leave
+exit 0 # Exit and leave
