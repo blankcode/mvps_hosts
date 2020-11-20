8d804deb0cac84d027351ab7c44d5f08ba75d709
Brian Blankenship <brianblankenship1978@gmail.com>
2020-11-20 16:11:55 -0500

    .

--- mvsp-update-new.sh
+++ mvsp-update-new.sh
@@ -12,11 +12,14 @@
 
 [[ -d /etc/MVP ]] && { mkdir /etc/MVP; }; # Make a tmp dir if not already there.
 cd /etc/MVP; # go to tmp dir.
+  # Sadly I don't see a way to cownload just a checksum so, we have to grab the file each time.
 curl http://winhelp2002.mvps.org/hosts.zip > hosts.zip; # download the hosts file from mvps and save to hosts.zip.
 unzip hosts.zip; # inflate hosts.zip.
-NEW_FILE=$(md5sum HOSTS); OLD_HOSTS=$(md5sum /etc/hosts); #Set the MD5SUMs from the files.
-[[ "$NEW_FILE" == "$OLD_HOSTS" ]] || { cat HOSTS > /etc/hosts; rm HOSTS hosts.zip }; # replace the /etc/hosts file with the new one if the new one is different.
-dscacheutil -flushcache; # reset the DNS Cache and initialize the new hosts file.
-unset NEW_FILE; unset OLD_HOSTS; #Clear the variables.
+NEW_FILE=$(md5sum HOSTS); OLD_HOSTS=$(md5sum /etc/hosts); # Set the MD5SUMs from the files.
+[[ "$NEW_FILE" == "$OLD_HOSTS" ]] || { 
+  cat HOSTS > /etc/hosts; rm HOSTS hosts.zip; # replace the /etc/hosts file with the new one if the new one is different.
+  dscacheutil -flushcache; # reset the DNS Cache and initialize the new hosts file.
+  unset NEW_FILE; unset OLD_HOSTS; # Clear the variables.
+}; 
 
 exit 0; # Exit well and leave.
